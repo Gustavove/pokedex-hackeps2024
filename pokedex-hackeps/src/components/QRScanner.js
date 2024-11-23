4de7972f4126119cc703ffb4e1dbdd2a3c9a635d
Gustavove <gustavove510@gmail.com>
2024-11-23 22:25:56 +0100

    Camara QR: Solo pilla el ID

--- pokedex-hackeps/src/components/QRScanner.js
+++ pokedex-hackeps/src/components/QRScanner.js
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 import { QrReader } from "react-qr-reader";
 
-const QRScanner = ({onScanResult}) => {
+const QRScanner = () => {
     const [data, setData] = useState("No se ha detectado ningún ID");
-    const [lastData, setLastData] = useState(""); // Estado para rastrear el último código leído
+    const [error, setError] = useState("");
 
     const handleScan = (result) => {
-        if (result?.text) {
-            const extractedId = result.text.split("/").pop();
-
-            if (extractedId !== lastData) {
-                setData(extractedId || "No se encontró un ID válido");
-                setLastData(extractedId);
-                onScanResult(extractedId);
-            }
+        if (result) {
+            const extractedId = result.text.split('/').pop();
+            setData(extractedId || "No se encontró un ID válido");
+            setError("");
         }
+    };
+
+    const handleError = (err) => {
+        console.error("Error al escanear:", err);
+        setError("Error al intentar leer el QR.");
     };
 
     return (
         <div style={{ textAlign: "center" }}>
+            <h2>Escanner de zonas</h2>
             <QrReader
-                onResult={(result) => {
-                    if (result?.text) {
+                onResult={(result, error) => {
+                    if (!!result) {
                         handleScan(result);
+                    }
+
+                    if (!!error) {
+                        handleError(error);
                     }
                 }}
                 style={{ width: "100%" }}
                 constraints={{ facingMode: "environment" }} // Cámara trasera
             />
             <p>Id de la zona: {data}</p>
+            {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
     );
 };
