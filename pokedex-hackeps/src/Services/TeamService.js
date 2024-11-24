b9c679c4cb207a6554f436b457d72fb713ac840a
alvarodr21 <alvarodr21@gmail.com>
2024-11-24 05:20:54 +0100

    evolution!!!

--- pokedex-hackeps/src/Services/TeamService.js
+++ pokedex-hackeps/src/Services/TeamService.js
@@ -1,10 +1,15 @@
 import { getById, getTeams } from '../Services/TeamGateway.js';
 
 export const getAllTeams = async () => {
-    return await getTeams();
+    return await getTeams().then(response => {return response.json()});
 }
 
 export const getTeam = async() => {
     const team_id = '28620274-0860-416a-baee-4ae42f8623fd'
-    return await getById(team_id)
+    return await getById(team_id).then(response => {return response.json()})
+}
+
+export const getTeamPokemons = async() => {
+    const team = await getTeam()
+    return team['captured_pokemons']
 }
