c96d723b6a1771b774a466facdcbb3bbe1843af2
nmayol <02mayolneus@gmail.com>
2024-11-24 06:07:55 +0100

    display teams ranking

--- pokedex-hackeps/src/services/TeamService.js
+++ pokedex-hackeps/src/services/TeamService.js
@@ -8,3 +8,9 @@ export const getTeam = async() => {
     const team_id = '28620274-0860-416a-baee-4ae42f8623fd'
     return await getById(team_id)
 }
+
+
+export const getTeamById = async (id) => {
+    return await getById(id);
+}
+
